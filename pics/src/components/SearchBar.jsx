9fa397a97e6fc39b6d8213aa3d848a2ae5779f59
Bong Lee <bongster228@github.com>
2019-12-02 02:01:48 -0800

    12-2-2019

--- pics/src/components/SearchBar.jsx
+++ pics/src/components/SearchBar.jsx
@@ -6,10 +6,17 @@ class SearchBar extends Component {
         term: ''
     };
 
+    onFormSubmit(event) {
+        //  prevent the page from refreshing
+        event.preventDefault();
+
+        console.log(this.state.term);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={(e) => this.onFormSubmit(e)} className="ui form">
                     <div className='field'>
                         <label>Image Search</label>
                         <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })} />
